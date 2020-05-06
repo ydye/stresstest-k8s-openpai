7bf1d816d10a4fd755fa141995b392b412c8d73d
YundongYe <YundongYe@outlook.com>
2020-05-06 17:15:21 +0800

    Issue

--- healthz.py
+++ healthz.py
@@ -3,7 +3,7 @@ from locust import HttpLocust, TaskSet, task, between
 class HealthZTask(TaskSet):
     @task
     def nginxHeartBeat(self):
-       self.client.get("/healthz", catch_response=True)
+       self.client.get("/healthz")
 
 class K8SAgent(HttpLocust):
     task_set = HealthZTask
