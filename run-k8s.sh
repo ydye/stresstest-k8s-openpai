8653c70951574e2f5b3e322b34b876ccf9f273a0
YundongYe <YundongYe@outlook.com>
2020-05-07 16:48:10 +0800

    Add service account in pod.

--- run-k8s.sh
+++ run-k8s.sh
@@ -17,7 +17,9 @@ echo "config file path: ${CLUSTER_CONFIG}"
 
 python3 stresstest-generator.py -c ${CLUSTER_CONFIG}
 
-kubectl create configmap locust-script-configuration --from-file=kubernetes/ --dry-run -o yaml | kubectl apply --overwrite=true -f - || exit $?
+kubectl create configmap locust-script-configuration --from-file=test/ --dry-run -o yaml | kubectl apply --overwrite=true -f - || exit $?
+
+kubectl create -f kubernetes/locust-sa.yaml
 
 kubectl create -f kubernetes/locust-master.yaml
 
