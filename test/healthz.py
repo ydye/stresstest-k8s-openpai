37f25af04258f55c35f489b046fe35401f95ff09
YundongYe <YundongYe@outlook.com>
2020-05-08 16:25:07 +0800

    For load test

--- test/healthz.py
+++ test/healthz.py
@@ -20,33 +20,33 @@ class HealthZTask(TaskSet):
     kube_cert = None
     headers = {}
 
-    def setup(self):
-        with open(SERVICE_TOKEN_FILENAME) as f:
-            self.kube_token = f.read()
-        self.kube_cert = SERVICE_CERT_FILENAME
-        self.headers = {
-            "Authorization": "Bearer {0}".format(self.kube_token)
-        }
+    #def setup(self):
+    #    with open(SERVICE_TOKEN_FILENAME) as f:
+    #        self.kube_token = f.read()
+    #    self.kube_cert = SERVICE_CERT_FILENAME
+    #    self.headers = {
+    #        "Authorization": "Bearer {0}".format(self.kube_token)
+    #    }
 
     @task
     def nginxHeartBeat(self):
         self.client.get("/healthz", verify=False)
 
-    @task
-    def getPodList(self):
-        self.client.get("/api/v1/pods", verify = self.kube_cert, headers = self.headers)
+    #@task
+    #def getPodList(self):
+    #    self.client.get("/api/v1/pods", verify = self.kube_cert, headers = self.headers)
 
-    @task
-    def getNodeList(self):
-        self.client.get("/api/v1/nodes", verify = self.kube_cert, headers = self.headers)
+    #@task
+    #def getNodeList(self):
+    #    self.client.get("/api/v1/nodes", verify = self.kube_cert, headers = self.headers)
 
 
 class K8SAgent(HttpLocust):
     task_set = HealthZTask
-    wait_time = between(10, 10)
+    wait_time = between(0, 0)
 
-    def setup(self):
-        self.host = (
-            "https://" + _join_host_port(os.environ[SERVICE_HOST_ENV_NAME],
-                                         os.environ[SERVICE_PORT_ENV_NAME]))
+    #def setup(self):
+        #self.host = (
+        #    "https://" + _join_host_port(os.environ[SERVICE_HOST_ENV_NAME],
+        #                                 os.environ[SERVICE_PORT_ENV_NAME]))
 
