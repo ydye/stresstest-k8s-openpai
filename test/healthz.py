29f10e73d91c3223f8f1d20dead03a4665f59c43
YundongYe <YundongYe@outlook.com>
2020-05-22 19:47:44 +0800

    Add docker file and test job

--- test/healthz.py
+++ test/healthz.py
@@ -1,6 +1,8 @@
 import os
 import requests
+import socket
 import yaml
+import jinja2
 from locust import HttpLocust, TaskSet, task, between
 
 SERVICE_HOST_ENV_NAME = "KUBERNETES_SERVICE_HOST"
@@ -16,17 +18,33 @@ def _join_host_port(host, port):
         template = "[%s]:%s"
     return template % (host, port)
 
+def read_template(template_path):
+
+    with open(template_path, "r") as f:
+        template_data = f.read().decode('utf-8')
+
+    return template_data
+
+def generate_from_template_dict(template_data, jobname):
+
+    generated_file = jinja2.Template(template_data).render(
+        {'jobname': jobname}
+    )
+
+    return generated_file
+
 class HealthZTask(TaskSet):
 
     kube_token = None
     kube_cert = None
     headers = {}
+    id = 0
 
     def setup(self):
         with open(SERVICE_TOKEN_FILENAME) as f:
             self.kube_token = f.read()
         self.kube_cert = SERVICE_CERT_FILENAME
-        self.headers = {
+        self.k8s_headers = {
             "Authorization": "Bearer {0}".format(self.kube_token)
         }
 
@@ -41,17 +59,37 @@ class HealthZTask(TaskSet):
         r = requests.post("{0}/rest-server/api/v2/authn/basic/login".format(self.pai_restserver), data = payload)
         self.pai_token = r.json()['token']
 
+        self.job_template = read_template("/mnt/locust/test-job.yml")
 
-    @task(1)
+    @task(5)
     def submitjob(self):
-        headers = {
+        hostname = socket.gethostbyname()
+        jobname = "stresstest-{0}-{1}".format(hostname, self.id)
+        self.id = id + 1
+
+        template_data = generate_from_template_dict(self.job_template, jobname)
+
+
+        openpai_headers = {
             "Authorization": "Bearer {0}".format(self.pai_token),
             "Content-Type": "text/yaml"
         }
 
         self.client.post(
             "/rest-server/api/v2/job",
-            headers=headers
+            headers=openpai_headers,
+            data=template_data
+        )
+
+
+    @task(1)
+    def listjoball(self):
+        openpai_headers = {
+            "Authorization": "Bearer {0}".format(self.pai_token),
+        }
+        self.client.get(
+            "/rest-server/api/v2/job",
+            headers=openpai_headers
         )
 
     #@task
