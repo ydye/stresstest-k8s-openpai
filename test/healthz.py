0eb65521765ebca39aaf8c4a9f4de807f76c30e8
YundongYe <YundongYe@outlook.com>
2020-06-02 10:16:43 +0800

    Update yaml file

--- test/healthz.py
+++ test/healthz.py
@@ -58,7 +58,7 @@ class K8SAgent(FastHttpUser):
     def on_start(self):
         self.userid = str(uuid.uuid4())
 
-    '''
+
     @task(1)
     def submitjob(self):
         hostname = os.environ['MY_POD_NAME']
@@ -79,7 +79,6 @@ class K8SAgent(FastHttpUser):
         )
 
     '''
-    '''
     @task(10)
     def listjoball(self):
         openpai_headers = {
@@ -91,7 +90,8 @@ class K8SAgent(FastHttpUser):
         )
 
     '''
+    '''
     @task(10)
     def getPodList(self):
         self.client.get(kube_url + "/api/v1/pods", verify = kube_cert, headers = k8s_headers)
-
+    '''
