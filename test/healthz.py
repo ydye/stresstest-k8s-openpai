e268b907e7520e022b9538bbfa03d2a30a4f5a58
YundongYe <YundongYe@outlook.com>
2020-06-10 14:41:38 +0800

    Merge branch 'master' of https://github.com/ydye/stresstest-k8s-openpai
    # Conflicts:
    #	test/healthz.py

--- test/healthz.py
+++ test/healthz.py
@@ -81,7 +81,7 @@ class K8SAgent(FastHttpUser):
             data=template_data
         )
     '''
-    '''
+
     @task(10)
     def listjoball(self):
         openpai_headers = {
@@ -97,4 +97,4 @@ class K8SAgent(FastHttpUser):
     @task(10)
     def getPodList(self):
         self.client.get(kube_url + "/api/v1/pods", verify = kube_cert, headers = k8s_headers)
-
+    '''
